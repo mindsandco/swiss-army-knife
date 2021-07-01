4cef2737ea958e7545cba7856070cf7cfe99911a
Gais El-Aasi <gea@scadaminds.com>
2021-07-01 10:32:59 +0200

    added examples in method documentations

--- Source/SCM.SwissArmyKnife/Compression/Gzip.cs
+++ Source/SCM.SwissArmyKnife/Compression/Gzip.cs
@@ -14,6 +14,14 @@ namespace SCM.SwissArmyKnife.Compression
         /// This method will compress a given byte array <paramref name="dataToCompress"/> using gzip.
         /// </summary>
         /// <param name="dataToCompress">The byte array that will be compressed.</param>
+        /// <example>
+        /// <code>
+        /// var myByteArray = new byte[] {1, 2, 3, 5, 6, 7 };
+        /// // Will return the compressed byte array
+        ///
+        /// var myCompressedByteArray = Gzip.Compress(myByteArray);
+        /// </code>
+        /// </example>
         /// <returns>The compressed byte array.</returns>
         [Pure]
         public static byte[] Compress(byte[] dataToCompress)
@@ -35,6 +43,17 @@ namespace SCM.SwissArmyKnife.Compression
         /// <remarks>If no encoding <paramref name="encodingToUse"/> it will use the default UTF8 encoding.</remarks>
         /// <param name="stringToCompress">String to compress.</param>
         /// <param name="encodingToUse">Enconding to use for converting string to byte array.</param>
+        /// <example>
+        /// <code>
+        /// var myString = "my string";
+        ///
+        /// // Will return the compressed string as byte array, using ASCII for encoding
+        /// var myCompressedByteArrayASCII = Gzip.Compress(myByteArray, Encoding.ASCII);
+        ///
+        /// // Will return the compressed string as byte array, using UTF8 for encoding
+        /// var myCompressedByteArrayUTF8 = Gzip.Compress(myByteArray);
+        /// </code>
+        /// </example>
         /// <returns>Compressed byte array.</returns>
         [Pure]
         public static byte[] Compress(string stringToCompress, Encoding? encodingToUse = null)
@@ -47,6 +66,14 @@ namespace SCM.SwissArmyKnife.Compression
         /// This method will decompress a given byte array <paramref name="dataToDecompress"/> using gzip.
         /// </summary>
         /// <param name="dataToDecompress">Byte array to be decompressed.</param>
+        /// <example>
+        /// <code>
+        /// var myCompressedByteArray = Gzip.Compress(new byte[] { 1, 2, 3, 5, 6, 7 });
+        ///
+        /// // Will return the decompressed byte array
+        /// var myDecompressedByteArray = Gzip.Decompress(myCompressedByteArray);
+        /// </code>
+        /// </example>
         /// <returns>The decompressed byte array.</returns>
         [Pure]
         public static byte[] Decompress(byte[] dataToDecompress)
@@ -66,6 +93,18 @@ namespace SCM.SwissArmyKnife.Compression
         /// <remarks>If no encoding <paramref name="encodingToUse"/> it will use the default UTF8 encoding.</remarks>
         /// <param name="dataToDecompress">Byte array to decompress.</param>
         /// <param name="encodingToUse">Enconding to use for converting byte array to string.</param>
+        /// <example>
+        /// <code>
+        /// var myCompressedStringASCII = Gzip.Compress("my string", Encoding.ASCII);
+        /// var myCompressedStringUTF8 = Gzip.Compress("me string");
+        ///
+        /// // Will return the decompressed string using the provided encoding
+        /// var myDecompressedStringASCII = Gzip.DecompressToString(myCompressedStringASCII, Encoding.ASCII);
+        ///
+        /// // Will return the decompressed string using the default encoding
+        /// var myDecompressedStringUTF8 = Gzip.DecompressToString(myCompressedStringUTF8);
+        /// </code>
+        /// </example>
         /// <returns>Decompressed string.</returns>
         [Pure]
         public static string DecompressToString(byte[] dataToDecompress, Encoding? encodingToUse = null)
